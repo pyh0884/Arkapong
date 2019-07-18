f9f1e7dee1b44218c691d79e459445bd5c6fb2e4
煌瞳小妖 <892611452@qq.com>
2019-07-18 19:55:08 +0800

    pong mode

--- Assets/Scripts/blocks/SimpleBlocks.cs
+++ Assets/Scripts/blocks/SimpleBlocks.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SimpleBlocks : Blocks
 {
-    private void OnDestroy() {
+
+    public override void DestroyThis(){ //子类无需再写OnCollision,不同砖块可重写被撞击后的代码
+        if(destroying)
+            return;
+        destroying = true;
         Instantiate(Resources.Load("BreakEffect"),transform.position,Quaternion.identity);
+        Destroy(gameObject);
     }
 
 }
