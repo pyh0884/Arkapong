b6551a8ad4a456e1744d1bee358f232befbc10b9
HenrySecrets <49709071+HenrySecrets@users.noreply.github.com>
2019-07-18 14:31:11 +0800

    Revert "Revert "2019.7.18 2:00""
    This reverts commit 78980ee97ca035b4f81a9cc1ac483650de1593c2.

--- Assets/Scripts/BuildingMode/NumberCheckerController.cs
+++ Assets/Scripts/BuildingMode/NumberCheckerController.cs
@@ -2,6 +2,7 @@
 public class NumberCheckerController : MonoBehaviour/*数量检查器控制器*/
 {
     public int[] remainedNumber;//砖块剩余数量
+    public GameObject[] numberPosition;//数量显示的位置
 
     private void Update()//每帧更新的部分
     {
@@ -11,12 +12,30 @@ public class NumberCheckerController : MonoBehaviour/*数量检查器控制器*/
             {
                 remainedNumber[(int)CheckBoxController.currentFunction]--;//砖块的数量自减
             }
+
+            switch (remainedNumber[(int)CheckBoxController.currentFunction])//查看剩余数量并替换相关图片
+            {
+                case 3://还剩3个
+                    numberPosition[(int)CheckBoxController.currentFunction].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BuildingMode/3");
+                    break;
+                case 2://还剩2个
+                    numberPosition[(int)CheckBoxController.currentFunction].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BuildingMode/2");
+                    break;
+                case 1://还剩1个
+                    numberPosition[(int)CheckBoxController.currentFunction].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BuildingMode/1");
+                    break;
+                case 0://还剩0个
+                    numberPosition[(int)CheckBoxController.currentFunction].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BuildingMode/0");
+                    break;
+            }
         }
 
+
+
         /*测试用*/
-        for (int i = 0; i < remainedNumber.Length; i++)
-        {
-            Debug.Log("Function " + (CheckBoxController.FunctionKind)i + ": "+remainedNumber[i]);
-        }
+        //for (int i = 0; i < remainedNumber.Length; i++)
+        //{
+        //    Debug.Log("Function " + (CheckBoxController.FunctionKind)i + ": "+remainedNumber[i]);
+        //}
     }
 }
